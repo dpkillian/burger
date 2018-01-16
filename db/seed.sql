0ff33fca9430aab38a9a92e5ff8f8678c48868b5
David Killian <dpkillian@icloud.com>
2018-01-16 15:24:06 -0500

    initial commit with empty folders and files

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+("Bacon Avacado", TRUE), 
+("Ranchero Supremo", TRUE), 
+("Swiss No-Miss", FALSE), 
+("Mad Hatter", TRUE), 
+("Chili Please", FALSE), 
+("Hot Tomorrow", FALSE), 
+("Triper Yummy", TRUE), 
+("Greasy Musician", FALSE);
