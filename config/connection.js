5a69dd8906dd369e0fb1f0be8b7c5f812341f192
David Killian <dpkillian@icloud.com>
2018-01-17 10:41:10 -0500

    converted cats.js to burgers.js, a lot of debugging to do

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,4 @@
+// Set up MySQL connection; either locally or via Heroku/Jaws DB
 var mysql = require("mysql");
 var connection;
 
@@ -5,9 +6,23 @@ if(process.env.JAWSDB_URL){
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
+	port: 3306,
 	host: "localhost",
 	user: "root",
 	password: "",
 	database: "burgers_db"
 	});
 };
+
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
